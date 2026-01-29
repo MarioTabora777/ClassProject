7ac762b53c758e96b3f04cdd9743da3a82a06566
MarioTabora777 <46462339+MarioTabora777@users.noreply.github.com>
2026-01-28 20:52:11 -0600

    props y estilización

--- App.tsx
+++ App.tsx
@@ -2,13 +2,28 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CustomButton from './source/components/CustomButton';
 
+
+
 //solo se puede retorar un componente,
 export default function App() {
+  const handleOnLogin =() => {
+console.log('Log In...')
+alert('Log In from app')
+  } 
+
+   const handleOnLogOut =() => {
+console.log('Log out ...') 
+alert('Log out from app')
+
+
+  }
   return (
     <View style={styles.container}>
       <Text>Hello world!</Text>
       <StatusBar style="auto" />
-      <CustomButton> </CustomButton>
+      <CustomButton title='Log In ...' onClick={handleOnLogin}></CustomButton> 
+      <CustomButton title='Log out ...' onClick={handleOnLogOut}></CustomButton>
+
     </View>
   );
 }
--- source/components/CustomButton.tsx
+++ source/components/CustomButton.tsx
@@ -1,13 +1,27 @@
 import React, {useState} from 'react';
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+interface CustomButtonProp {
+title:string;
+onClick: ()=>void
 
-export default function CustomButton(){ 
+}
+
+export default function CustomButton({title,onClick}:CustomButtonProp ){ 
  return (
     <View>
-     <TouchableOpacity>
-          <Text>Press Here!!!</Text>
+     <TouchableOpacity onPress={onClick} style ={style.container}>
+          <Text>{title}</Text>
         </TouchableOpacity>
       
     </View>
   );
 } ;
+
+const style = StyleSheet.create({
+  container : {
+
+    backgroundColor: "#ebd8e1"
+
+  }
+
+});
